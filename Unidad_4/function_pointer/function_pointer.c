d15f2b055264d0054d293c1194cd99fe8f661231
Worm <wormy318@gmail.com>
2017-10-03 19:07:30 -0300

    puntero a funcion

---
+++ Unidad_4/function_pointer/function_pointer.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+void ejecutar(void fun(int*),int *a){
+  (*fun)(a);
+}
+
+void sumarUno(int *a){
+  *a += 1;
+}
+
+void restarUno(int *a){
+  *a -= 1;
+}
+
+
+int main(){
+  int a = 3;
+  ejecutar(&sumarUno,&a);
+  printf("%d\n",a);
+  ejecutar(&restarUno,&a);
+  ejecutar(&restarUno,&a);
+  printf("%d\n",a);
+
+  return 0;
+}
