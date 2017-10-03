7977d82cf26eac760fac3b2a24be9a342654a059
Worm <wormy318@gmail.com>
2017-10-03 18:54:46 -0300

    ejemplo puntero a void

--- Unidad_3/void_pointers/void_pointer.c
+++ Unidad_3/void_pointers/void_pointer.c
@@ -1,15 +1,29 @@
 #include <stdio.h>
 
-void funcionMostrar(void* a){
-
+void funcionMostrar(int a){
+  printf("%d\n",a);
 }
+
+void funcionMostrar(float a){
+  printf("%f\n");
+}
+
+// void funcionMostrar(void* a, int tipo){
+//   if(tipo == 0){
+//     printf("%d\n",*(int*)a);
+//   }else{
+//     printf("%f\n",*(double*)a);
+//   }
+// }
 
 int main(){
   int i = 5;
   double f = 9.7;
 
-  void*
-
+  void* v = &f;
+  funcionMostrar(v,1);
+  v = &i;
+  funcionMostrar(v,0);
 
   return 0;
 }
---
+++ Unidad_3/void_pointers/void_structs.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Foo {
+  void *a;
+  int tipo; //quizas se mejor con un Enum
+};
+
+void mostrarDato(struct Foo f){
+  switch(f.tipo){
+    case 0: //int
+      printf("%d\n",*(int*)f.a);
+      break;
+    case 1: //char
+      printf("%c\n",*(char*)f.a);
+      break;
+    case 2: //double
+      printf("%f\n",*(double*)f.a);
+      break;
+    default:
+      printf("Tipo no valido\n");
+
+  }
+}
+
+int main(){
+  struct Foo f;
+
+  f.a = malloc(sizeof(int));
+  f.tipo = 0;
+  *(int*)f.a = 5;
+  mostrarDato(f);
+
+  f.a = realloc(f.a,sizeof(char));
+  f.tipo = 1;
+  *(char*)f.a = 'L';
+  mostrarDato(f);
+
+  f.a = realloc(f.a,sizeof(double));
+  f.tipo = 2;
+  *(double*)f.a = 9.7;
+  mostrarDato(f);
+
+  free(f.a);
+
+  return 0;
+}
