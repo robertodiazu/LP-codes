2e7a84f334bd7e4e25954c81d4a28c5da6968cab
Worm <wormy318@gmail.com>
2017-12-06 11:21:32 -0300

    esto es mas simple y entendible

--- Unidad_8/Guia de Ejercicios/Ejercicio4rd.pl
+++ Unidad_8/Guia de Ejercicios/Ejercicio4rd.pl
@@ -4,9 +4,9 @@ mejor(A,B,PER):-
     PER=[_ , A , B].
 
 puzzle(A):-
-    PER = [[Nom1,primero,cricket,Nac1],[Nom2,segundo,Dep2,Nac2],[Nom3,tercero,Dep3,Nac3]],
-    mejor([michael,_,futbol,_],[_,_,_,usa],PER),
-    mejor([simon,_,_,israel],[_,_,tenis,_],PER),
+    PER = [persona(Nom1,primero,cricket,Nac1),persona(Nom2,segundo,Dep2,Nac2),persona(Nom3,tercero,Dep3,Nac3)],
+    mejor(persona(michael,_,futbol,_),persona(_,_,_,usa),PER),
+    mejor(persona(simon,_,_,israel),persona(_,_,tenis,_),PER),
     member(Nom1,[michael,simon,richard]),
     member(Nom2,[michael,simon,richard]),
     member(Nom3,[michael,simon,richard]),
@@ -15,7 +15,7 @@ puzzle(A):-
     member(Nac1,[israel,australia,usa]),
     member(Nac2,[israel,australia,usa]),
     member(Nac3,[israel,australia,usa]),
-    not(Nom1 = Nom2), not(Nom2 = Nom3), not(Nom1 = Nom3),
-    not(Dep2 = Dep3),
-    not(Nac1 = Nac2), not(Nac2 = Nac3), not(Nac1 = Nac3),
+    Nom1 \= Nom2, Nom2 \= Nom3, Nom1 \= Nom3,
+    Dep2 \= Dep3,
+    Nac1 \= Nac2, Nac2 \= Nac3, Nac1 \= Nac3,
     member(A,PER).
