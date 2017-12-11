4e948ab08b5eff2a68e297a1b5b5a918e2bae3ab
Worm318 <wormy318@gmail.com>
2017-12-11 15:28:03 -0300

    ejercicio guia

---
+++ Unidad_8/GuiaC2Ex4.scm
@@ -0,0 +1,23 @@
+#lang scheme
+
+(define (maximo ls)
+  (apply max ls)
+  )
+
+(define (maxM ls)
+  (let maxM-aux ([ls (cdr ls)] [actual-max (car ls)])
+    (if (null? ls) actual-max
+    (if (list? (car ls))
+        (if (> (maximo (car ls)) actual-max)
+            (maxM-aux (cdr ls) (maximo (car ls)))
+            (maxM-aux (cdr ls) actual-max)
+            )
+        (if (> (car ls) actual-max)
+            (maxM-aux (cdr ls) (car ls))
+            (maxM-aux (cdr ls) actual-max)
+            )
+    )
+    )
+  )
+  )
+
